cc4f9ab7380884ac63e3e939fc00b98d5ba39610
Wei Zhang <veeamd@gmail.com>
2021-07-26 22:24:04 -0400

    Initial Commit

---
+++ Algo/AlgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoApp.swift
+//  Algo
+//
+//  Created by Wei Zhang on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AlgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
